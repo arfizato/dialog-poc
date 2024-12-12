d021c4bbc02f7cff3643e48dda27fde8ca767156
Arfizato <33662900+arfizato@users.noreply.github.com>
2024-12-13 00:53:02 +0100

    feat: full dialog

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,14 +6,16 @@ import { Dialog } from "./ui/Dialog";
 export default function Home() {
 	const dialog = useRef<HTMLDialogElement>(null);
 	return (
-		<div className='popupTrigger__wrapper'>
-			<button
-				onClick={() => {
-					dialog.current?.show();
-				}}>
-				Do not click me!
-			</button>
+		<>
 			<Dialog ref={dialog}></Dialog>
-		</div>
+			<div className='popupTrigger__wrapper'>
+				<button
+					onClick={() => {
+						dialog.current?.showModal();
+					}}>
+					Do not click me!
+				</button>
+			</div>
+		</>
 	);
 }
